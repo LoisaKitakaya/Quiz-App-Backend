1db6d4e36d381315e91b7460f53c327b9937bf77
Freedom Loisa <kitakayaloisa@gmail.com>
2024-12-02 19:39:58 +0300

    adding alex into the circle

--- company/apis/api_v1.py
+++ company/apis/api_v1.py
@@ -15,7 +15,7 @@ def contact_us(request, data: ContactForm):
             message=f"Hello Alex! A client has made contact with you. The client's name is: {data.first_name} {data.last_name}. Email address: {data.email}. The client's message is: '{data.message}'. Make sure the client is responded to.",
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[
-                # "agiebner10@gmail.com",
+                "agiebner10@gmail.com",
                 "kitakayaloisa@gmail.com",
             ],
             fail_silently=True,
